3cfb559e8e54e4683326f96ae803062c26a5874e
Anastasia <asya_grigor@mail.ru>
2021-11-19 14:22:59 +0300

    Добавлены MiniCssExtractPlugin, alias, babel, postcss, jquery

--- webpack.common.js
+++ webpack.common.js
@@ -1,52 +1,95 @@
-const path= require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } =require('clean-webpack-plugin')
-const webpack = require('webpack')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname,'./src/js/index.js'),
-        hot: 'webpack/hot/dev-server.js',
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].[contenthash].bundle.js',
+  entry: {
+    main: path.resolve(__dirname, "./src/js/index.js"),
+    hot: "webpack/hot/dev-server.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].bundle.js",
+    assetModuleFilename: "assets/[hash][ext][query]",
+  },
+  resolve:{
+    alias:{
+      '@': path.resolve(__dirname,'src'),
+      '@styles': path.resolve(__dirname,'src/styles/'),
+      '@ui-kit': path.resolve(__dirname,'src/layout/ui-kit/'),
+      '@blocks': path.resolve(__dirname,'src/blocks/'),
+    }
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.pug$/,
-                use: [
-                    { loader: "pug-loader" }
-                ]
+      },
+      {
+        test: /\.pug$/,
+        use: [
+          {
+            loader: "pug-loader",
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: ["postcss-preset-env"],
+              },
             },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [ 
-                    { loader: "style-loader" },
-                    { loader: "css-loader" } ,
-                    { loader: "sass-loader" }
-                ]        
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ]
-    },    
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/layout/ui-kit/colors-type/colors-type.pug'), 
-            filename: 'index.html',
-        }),        
-        new webpack.HotModuleReplacementPlugin(),        
-        new webpack.SourceMapDevToolPlugin(),
-        new CleanWebpackPlugin(),
-    ]      
-    
-}
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(
+        __dirname,
+        "./src/layout/ui-kit/form-elements/form-elements.pug"
+      ),
+      filename: "index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.SourceMapDevToolPlugin(),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "styles.css" }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.$': 'jquery',
+      'window.jQuery': 'jquery',
+    }),    
+  ],
+};
