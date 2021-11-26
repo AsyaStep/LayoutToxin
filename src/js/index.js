d7d737a414fc300116db56d0bee15ecb20487ded
Anastasia <asya_grigor@mail.ru>
2021-11-26 14:23:28 +0300

    Добавлен импорт блока dropdown

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,7 @@ import '@styles/index.scss';
 import '@ui-kit/colors-type/colors-type.scss';
 import '@blocks/field-text/field-text.scss';
 import '@blocks/field-text/field-text.js';
+import '@blocks/dropdown/dropdown.scss';
+import '@blocks/dropdown/dropdown.js';
 
  
