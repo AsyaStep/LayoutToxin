57012febf1198d5021e66ac3bfb9c4caa8864ced
Anastasia <asya_grigor@mail.ru>
2021-12-24 00:29:06 +0300

    При добавлении ребенка/младенца автоматически добавляется взрослый, если он был равен 0

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -1,6 +1,7 @@
 let input = document.querySelector(".dropdown__input");
 let textInput = document.querySelector(".dropdown__content");
 let menu = document.querySelector(".dropdown__menu");
+let divQuantity = document.querySelectorAll(".dropdown__quantity"); 
 let btnDecrement = document.querySelectorAll(".dropdown__decrement");
 let btnIncrement = document.querySelectorAll(".dropdown__increment");
 
@@ -13,6 +14,24 @@ let quantityBaby = 0;
 let selectionText = {
   man: ['гость', 'гостя', 'гостей'],
   baby: ['младенец', 'младенца', 'младенцев']
+}
+
+let quantityAdult = () => {
+  let quantityGuest = document.querySelectorAll(".dropdown__quantity-guest");
+  for (const i of quantityGuest){
+    if (i.dataset.id === 'adult'){
+      return Number(i.querySelector(".dropdown__quantity").textContent);      
+    }
+  }
+}
+
+let quantityAdultPlus = () => {
+  let quantityGuest = document.querySelectorAll(".dropdown__quantity-guest");
+  for (const i of quantityGuest){
+    if (i.dataset.id === 'adult'){
+      return i.querySelector(".dropdown__quantity").textContent = '1';      
+    }
+  }
 }
 
 let bendWords = (quantity, text) => { 
@@ -36,8 +55,7 @@ let bendWords = (quantity, text) => {
   }
 }
 
-let changeInput = (totalQuantity, quantityBaby) => {  
-  console.log(totalQuantity, quantityBaby);
+let changeInput = (totalQuantity, quantityBaby) => {
   if (totalQuantity != 0){ 
     textInput.textContent = String(totalQuantity) + ' ' + bendWords(totalQuantity, selectionText.man)        
   } 
@@ -52,36 +70,62 @@ let changeInput = (totalQuantity, quantityBaby) => {
 let quantityDecrement = (item) => {
     let quantity = item.parentElement.querySelector(".dropdown__quantity");
     let idOption = item.parentElement.dataset.id;    
-    let count = Number(quantity.textContent)
-  
+    let count = Number(quantity.textContent);
+
     if (count > minQuantity){
       count -= 1;
       quantity.textContent = String(count);
-      if (idOption != 'baby'){
-        totalQuantity -= 1;
-      }else {
-        quantityBaby -=1;
+      let adult = quantityAdult();
+      
+      if (adult === 0){
+        for ( const i of divQuantity){
+          i.textContent = '0'
+        }
+        totalQuantity = 0;
+        quantityBaby = 0;
+        changeInput(totalQuantity,quantityBaby); 
+      } else {
+        if (idOption != 'baby'){
+          totalQuantity -= 1;
+        } else {
+          quantityBaby -=1;
+        }
+        changeInput(totalQuantity,quantityBaby);
       }
-      changeInput(totalQuantity,quantityBaby);      
-      }   
+            
+    }   
 };
 
 let quantityIncrement = (item) => {
   let quantity = item.parentElement.querySelector(".dropdown__quantity");
   let idOption = item.parentElement.dataset.id;
-  let count = Number(quantity.textContent)
+  let count = Number(quantity.textContent)  
 
   let matchCount = (maxQuantity) => {
     if (count < maxQuantity){
       count+= 1;
       quantity.textContent = String(count)
-      if (idOption != 'baby'){
-        totalQuantity += 1;
+      let adult = quantityAdult();
+
+      if (idOption != 'adult' && adult === 0){
+        quantityAdultPlus();
+        quantity.textContent = String(count);
+        if (idOption === 'child'){
+          totalQuantity +=2;
+        } else {
+          totalQuantity +=1;
+          quantityBaby +=1;
+        }        
         changeInput(totalQuantity,quantityBaby);
       } else {
-        quantityBaby +=1;
-        changeInput(totalQuantity,quantityBaby);
-      }
+        if (idOption != 'baby'){
+          totalQuantity += 1;
+          changeInput(totalQuantity,quantityBaby);
+        } else {
+          quantityBaby +=1;
+          changeInput(totalQuantity,quantityBaby);
+        }
+      }      
     };
   }
   switch (idOption){
@@ -102,14 +146,14 @@ input.addEventListener("click",function(){
 })
 
 for (const item of btnDecrement){
-  item.addEventListener("click", function(){
-    quantityDecrement(item);
+  item.addEventListener("click", function(){        
+    quantityDecrement(item);    
   })
 }
 
 for (const item of btnIncrement){
   item.addEventListener("click", function(){
-    quantityIncrement(item)
+    quantityIncrement(item);
   })
 }
 
