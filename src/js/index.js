d6a34405f192ded9fe408ecc743b115457181dd2
Anastasia <asya_grigor@mail.ru>
2022-01-23 11:14:03 +0300

    import (button/toggle/checkbox).scss

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,8 @@ import '@blocks/field-text/field-text.scss';
 import '@blocks/field-text/field-text.js';
 import '@blocks/dropdown/dropdown.scss';
 import '@blocks/dropdown/dropdown.js';
+import '@blocks/button/button.scss';
+import '@blocks/toggle/toggle.scss';
+import '@blocks/checkbox/checkbox.scss';
 
  
